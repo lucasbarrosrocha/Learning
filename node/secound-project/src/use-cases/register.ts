2835ad218bb7f5282a410fb2f33bd9b6a871eb16
Lucas Barros <lucas.rocha@ccc.ufcg.edu.br>
2024-05-24 09:23:00 -0300

    new projects

---
+++ node/secound-project/src/use-cases/register.ts
@@ -0,0 +1,38 @@
+import { UsersRepository } from "@/repositories/users-repository"
+import { hash } from "bcryptjs"
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error"
+import { User } from "@prisma/client"
+
+interface registerUseCaseRequest {
+    name: string
+    email: string
+    password: string
+}
+
+interface RegisterUseCaseResponse {
+    user: User
+}
+
+export class RegisterUseCase {
+    constructor(private userRepository: UsersRepository) { }
+
+    async execute({ name, email, password }: registerUseCaseRequest): Promise<RegisterUseCaseResponse> {
+        const userWithSameEmail = await this.userRepository.findByEmail(email)
+
+        if (userWithSameEmail) {
+            throw new UserAlreadyExistsError()
+        }
+
+        const password_hash = await hash(password, 6)
+
+        const user = await this.userRepository.create({
+            name,
+            email,
+            password_hash,
+        })
+
+        return {
+            user
+        }
+    }
+}
