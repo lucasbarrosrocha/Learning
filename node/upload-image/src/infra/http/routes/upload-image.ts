4371369983a4a9e84b912fa97fe73a08ae31d15c
Lucas Barros <lucasbarros@Lucass-MacBook-Pro.local>
2025-04-08 08:48:43 -0300

    finalizando setup

---
+++ node/upload-image/src/infra/http/routes/upload-image.ts
@@ -0,0 +1,7 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+
+export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
+  server.post('/upload', () => {
+    return 'Hello World'
+  })
+}
--- node/upload-image/src/infra/http/server.ts
+++ node/upload-image/src/infra/http/server.ts
@@ -1,9 +1,58 @@
+import { env } from '@/env'
 import { fastifyCors } from '@fastify/cors'
+import fastifyMultipart from '@fastify/multipart'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { uploadImageRoute } from './routes/upload-image'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, _, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+
+  // enviar o erro p/ alguma ferramenta de observabilidade (Sentry/Datadog/Grafana/OTel)
+
+  console.error(error)
+
+  return reply.status(500).send({
+    message: 'Internal server error',
+  })
+})
+
 server.register(fastifyCors, { origin: '*' })
+
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Upload Server',
+      description: 'API for uploading images',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+server.register(uploadImageRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running on http://localhost:3333')
